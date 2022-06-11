20f574d328dc54fa9e99b9b8b43d95a0bda93ca9
Turtleion <97813862+turtleion@users.noreply.github.com>
2022-06-11 12:45:39 +0700

    Update onPlayerDisconnect.php

--- src/turtledev/MoneyGen/Event/onPlayerDisconnect.php
+++ src/turtledev/MoneyGen/Event/onPlayerDisconnect.php
@@ -9,27 +9,28 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerQuitEvent;
 
 class onPlayerDisconnect implements Listener {
+	protected $plugin;
 	public function __construct(MoneyGen $mg){
-		parent::__construct($mg);
+		$this->plugin = $mg;
 	}
 
 	public function onQuit(PlayerQuitEvent $event){
-		if($this->getCD()[$event->getPlayer()->getName()] == null){
-			$this->ignore();
+		if($this->plugin->getCD()[$event->getPlayer()->getName()] == null){
+			$this->plugin->ignore();
 		}else{
 			
 			$playername = $event->getPlayer()->getName();
-     	   		$getTime = $this->getCD()[$playername];
-    		    	$res = $db->query("INSERT INTO data VALUES ('$playername',$getTime)");
+     	   		$getTime = $this->plugin->getCD()[$playername];
+    		    	$res = $this->plugin->db->query("INSERT INTO data VALUES ('$playername',$getTime)");
         		if(!$res instanceof \SQLite3Result){
             			$adminPlayer = [];
-            			forech($this->getServer()->getOnlinePlayers() as $player){
+            			forech($this->plugin->getServer()->getOnlinePlayers() as $player){
                 			if($player->isOp()){
                     			array_push($adminPlayer, $player->getName());
                 		}
             		}
             		for($i = 0; i < count($adminPlayer); $i++){
-                		$this->getServer()->getPlayerByName($adminPlayer[i])->sendMessage("[*DEBUG*] Player " . $playername . " has disconnected but the timer is does'nt successfully saved!");
+                		$this->plugin->getServer()->getPlayerByName($adminPlayer[i])->sendMessage("[*DEBUG*] Player " . $playername . " has disconnected but the timer is does'nt successfully saved!");
             		}
 		}
 
