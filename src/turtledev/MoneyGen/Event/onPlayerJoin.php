56bfc29e7f24baadbc6bee3f609357c77bcfcb2d
Turtleion <97813862+turtleion@users.noreply.github.com>
2022-06-11 12:37:29 +0700

    Update onPlayerJoin.php

--- src/turtledev/MoneyGen/Event/onPlayerJoin.php
+++ src/turtledev/MoneyGen/Event/onPlayerJoin.php
@@ -6,14 +6,15 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
 
 class onPlayerJoin implements Listener {
+    protected $plugin;
     public function __construct(MoneyGen $m){
-        parent::__construct($m);
+        $this->plugin = $m;
     }
 
     public function onJoin(PlayerJoinEvent $event){
         $player = $event->getPlayer();
         $name = $player->getName();
-        $query = $this->db->query("SELECT * FROM data WHERE name = '$name'");
+        $query = $this->plugin->db->query("SELECT * FROM data WHERE name = '$name'");
         if(!$query instanceof \SQLite3Result){
             throw new Exception("Error in querying database");
         }
@@ -22,15 +23,15 @@ class onPlayerJoin implements Listener {
         if($cd == null){
             $cdt = $this->getCD();
             $c = $cdt += [$name => 5];
-            $this->setCD($c);
+            $this->plugin->setCD($c);
         }
-        if($this->getCD()[$name] == null){
+        if($this->plugin->getCD()[$name] == null){
             $t = $this->getCD() += [$name => $cd];
-            $this->setCD($t);
+            $this->plugin->setCD($t);
         }else{
             $t = $this->getCD();
             $t[$name] = $cd;
-            $this->setCD($t);
+            $this->plugin->setCD($t);
         }
     }
 
