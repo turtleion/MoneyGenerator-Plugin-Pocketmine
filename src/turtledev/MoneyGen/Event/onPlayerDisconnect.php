03e9c226ea55d2c85328bf8dd48892e188ad128c
Turtleion <97813862+turtleion@users.noreply.github.com>
2022-06-11 12:23:20 +0700

    Update onPlayerDisconnect.php

--- src/turtledev/MoneyGen/Event/onPlayerDisconnect.php
+++ src/turtledev/MoneyGen/Event/onPlayerDisconnect.php
@@ -14,19 +14,24 @@ class onPlayerDisconnect implements Listener {
 	}
 
 	public function onQuit(PlayerQuitEvent $event){
-        $playername = $event->getPlayer()->getName();
-        $getTime = $this->getTimer()[$playername];
-        $res = $db->query("INSERT INTO data VALUES ('$playername',$getTime)");
-        if(!$res instanceof \SQLite3Result){
-            $adminPlayer = [];
-            forech($this->getServer()->getOnlinePlayers() as $player){
-                if($player->hasPermission("core.admin")){
-                    array_push($adminPlayer, $player->getName());
-                }
-            }
-            for($i = 0; i < count($adminPlayer); $i++){
-                $this->getServer()->getPlayerByName($adminPlayer[i])->sendMessage("[*DEBUG*] Player " . $playername . " has disconnected but the timer is does'nt successfully saved!");
-            }
+		if($this->getCD()[$event->getPlayer()->getName()] == null){
+			$this->ignore();
+		}else{
+			
+			$playername = $event->getPlayer()->getName();
+     	   		$getTime = $this->getCD()[$playername];
+    		    	$res = $db->query("INSERT INTO data VALUES ('$playername',$getTime)");
+        		if(!$res instanceof \SQLite3Result){
+            			$adminPlayer = [];
+            			forech($this->getServer()->getOnlinePlayers() as $player){
+                			if($player->isOp()){
+                    			array_push($adminPlayer, $player->getName());
+                		}
+            		}
+            		for($i = 0; i < count($adminPlayer); $i++){
+                		$this->getServer()->getPlayerByName($adminPlayer[i])->sendMessage("[*DEBUG*] Player " . $playername . " has disconnected but the timer is does'nt successfully saved!");
+            		}
+		}
 
         }
     }
